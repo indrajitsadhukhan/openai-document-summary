500d3c6229cd011fcdc38629b314af820e417b38
indrajitsadhukhan <indrajitsadhukhan1000@gmail.com>
2023-04-28 11:23:17 +0530

    Fixed bugs

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.post('/completion', async (req, res) => {
       model: "text-davinci-003",
       prompt: `${prompt}`,
       temperature: 0,
-      max_tokens: 300, 
+      max_tokens: 2000, 
       top_p: 1, 
       frequency_penalty: 0.5,
       presence_penalty: 0, 
--- src/QuestionComp/QuestionComp.js
+++ src/QuestionComp/QuestionComp.js
@@ -2,41 +2,69 @@ import React, { Component,useState } from "react";
 import { Button } from "react-bootstrap";
 // import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
-import { API_URL } from '../constants'
+// import { API_URL } from '../constants'
 import './QuestionComp.scss'
+import fs from 'fs';
+const API_URL = "https://main--visionary-youtiao-038df7.netlify.app/.netlify/functions/api"
 
 const QuestionComp = ({setQuestion,question,content,setOutputText}) => {  
     const [text,setText]=useState("")  
     async function openAI_API(content,task)
-  {
-      // TODO: Import baseURL of API from config file.
-      const response = await fetch(API_URL+'/completion', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-              prompt: task
-          })
-      })
-      if(response.ok){
-      const data = await response.json();
-      const parsedData = data.bot
-      console.log(parsedData)
-      return parsedData;
-      }
-      console.log(response)
-      return "Error: API call failed. Try again.."
+    {
+      console.log(content + task)
+        const response = await fetch(API_URL + "/completion", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              prompt: content + task,
+            }),
+          }).catch((err) => console.log(err));
+          if (response?.ok) {
+            const data = await response.json();
+            const parsedData = data.bot;
+            return parsedData;
+          } else return "Failed! Try again";
+
+    // console.log(content + task)
+    // var text = content + task
+    // text = text.replace(/(\r\n|\n|\r)/gm,"")
+    // console.log(text)
+    //   // TODO: Import baseURL of API from config file.
+    //   const response = await fetch(API_URL+'/completion', {
+    //       method: 'POST',
+    //       headers: {
+    //           'Content-Type': 'application/json',
+    //       },
+    //       body: JSON.stringify({
+    //           prompt: text
+    //       })
+    //   })
+    //   if(response.ok){
+    //   const data = await response.json();
+    //   const parsedData = data.bot
+    //   console.log(parsedData)
+    //   return parsedData;
+    //   }
+    //   console.log(response)
+    //   return "Error: API call failed. Try again.."
   
   }
+
+
+
+
+
     async function handleButtonClick()
     {
         // Call API with content + question
         setQuestion(text)
         setOutputText("Processing...")
         // Show the text in OUTPUT
+        // console.log(content)
         const result=await openAI_API(content,text)
-        console.log(result)
+        // console.log(result)
         setOutputText(result)
 
     }
